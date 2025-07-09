71d4069968a1cc20049cdb787079a6e0f6a610e5
Andre Markov <andremarkov@icloud.com>
2025-07-09 17:16:21 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // import LabelEditor from "@/components/LabelEditor.vue";
 import LabelEditorTest from "@/components/LabelEditorTest.vue";
-import DataMatrixGS1 from "@/components/DataMatrixGS1.vue";
+import DataMatrixGS1 from "@/components/DataMatrixGS1.vue"; // TODO под замену т.к fnci не верный! 22 точки нужно, а у нас хрень
 import BarcodeGenerator from "@/components/BarcodeGenerator.vue";
 
 </script>
