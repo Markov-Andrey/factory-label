fea3ef7fef4e032f4e1e26214ba3ccd68574f6b9
Andre Markov <andremarkov@icloud.com>
2025-07-10 16:08:41 +0300

    add qr

--- src/components/LabelEditorTest.vue
+++ src/components/LabelEditorTest.vue
@@ -15,7 +15,7 @@
             <BaseButton @click="() => $refs.imageInput.click()" color="bg-purple-600" icon="PlusCircleIcon">Изображение</BaseButton>
             <input @change="addImageFromFile" type="file" ref="imageInput" class="hidden" accept="image/*" />
             <SelectGalleryIcons button-text="Иконка" button-color="bg-blue-600" button-icon="PlusCircleIcon" modal-title="Выберите иконку" :icons="fabricIconsSpecial()" @icon-selected="icon => addImage(this.canvas, icon.path, icon.meta)" />
-            <SelectGalleryIcons button-text="Код" button-color="bg-blue-600" button-icon="PlusCircleIcon" modal-title="Выберите баркод" :icons="fabricIconsBarcodes()" @icon-selected="icon => addImage(this.canvas, icon.path, icon.meta)" />
+            <SelectGalleryIcons button-text="Код" button-color="bg-blue-600" button-icon="PlusCircleIcon" modal-title="Выберите код" :icons="fabricIconsBarcodes()" @icon-selected="icon => addImage(this.canvas, icon.path, icon.meta)" />
         </div>
 
         <div class="flex gap-2">
