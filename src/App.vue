59a176617aea72c878a8cba57735ad6fb4ad878e
Andre Markov <andremarkov@icloud.com>
2025-07-11 15:24:51 +0300

    remove barcode generator

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 // import LabelEditor from "@/components/LabelEditor.vue";
 import LabelEditorTest from "@/components/LabelEditorTest.vue";
 import DataMatrixGS1 from "@/components/DataMatrixGS1.vue"; // TODO под замену т.к fnci не верный! 22 точки нужно, а у нас хрень
-import BarcodeGenerator from "@/components/BarcodeGenerator.vue";
 
 </script>
 
