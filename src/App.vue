804b03e61afe4387f88ace893f547b67410911d4
Andre Markov <andremarkov@icloud.com>
2025-07-12 08:03:00 +0300

    remove bwip-js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-// import LabelEditor from "@/components/LabelEditor.vue";
 import LabelEditorTest from "@/components/LabelEditorTest.vue";
-import DataMatrixGS1 from "@/components/DataMatrixGS1.vue"; // TODO под замену т.к fnci не верный! 22 точки нужно, а у нас хрень
 
 </script>
 
